cd0f4683c1ab83fb2474eb9e1b1f126033e5119f
USERSAD\fabnunez <fabnunez@everis.com>
2022-07-21 17:06:01 -0300

    peticion api imagenes

--- src/app/home/body/body.component.ts
+++ src/app/home/body/body.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { ComidaPaises } from 'src/app/shared/service/paises/models/comidaPaises';
+import { ImageDetail, ImagenesComida } from 'src/app/shared/service/paises/models/imagenesComida';
 import { Paises } from 'src/app/shared/service/paises/models/paises';
 import { PaisesService } from 'src/app/shared/service/paises/paises.service';
 import { SharedService } from 'src/app/shared/service/shared/shared.service';
@@ -8,42 +9,56 @@ import { SharedService } from 'src/app/shared/service/shared/shared.service';
 @Component({
   selector: 'app-body',
   templateUrl: './body.component.html',
-  styleUrls: ['./body.component.scss']
+  styleUrls: ['./body.component.scss'],
 })
 export class BodyComponent implements OnInit, OnDestroy {
-
   paisElegido!: Paises;
   paisComida!: ComidaPaises;
+  imagenesComida: ImageDetail[] = [];
   subscripcionPaisSeleccionado!: Subscription;
   mostrarMensaje: boolean = false;
-  constructor(private sharedService: SharedService, private paisesService: PaisesService) {
+  constructor(
+    private sharedService: SharedService,
+    private paisesService: PaisesService
+  ) {
     this.paisElegido = this.sharedService.paisElegido;
     this.obtenerComidaPais(this.paisElegido.name);
-    this.subscripcionPaisSeleccionado = this.sharedService.paisSeleccionado.subscribe(x =>{
-      this.paisElegido = x;
-      this.obtenerComidaPais(x.name)
-    })
+    this.subscripcionPaisSeleccionado =
+      this.sharedService.paisSeleccionado.subscribe((x) => {
+        this.imagenesComida = [];
+        this.paisElegido = x;
+        this.obtenerComidaPais(x.name);
+      });
   }
   ngOnDestroy(): void {
     this.subscripcionPaisSeleccionado.unsubscribe();
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  mostrarMensajeWikipedia(botonClickeado: boolean){
+  mostrarMensajeWikipedia(botonClickeado: boolean) {
     this.mostrarMensaje = botonClickeado;
     setTimeout(() => {
       this.mostrarMensaje = false;
     }, 4000);
   }
 
-  obtenerComidaPais(nombrePais: string){
-    this.paisesService.obtenerComidaPaises(nombrePais).subscribe(x => {
-      if(x){
+  obtenerComidaPais(nombrePais: string) {
+    this.paisesService.obtenerComidaPaises(nombrePais).subscribe((x) => {
+      if (x) {
         this.paisComida = x;
+        if(x.dish != ''){
+          this.obtenerImagenesComida(x.dish.toLocaleLowerCase(),x.country.toLocaleLowerCase());
+        }
       }
-    })
+    });
   }
 
+  obtenerImagenesComida(comida: string, pais: string){
+    this.paisesService.obtenerImagenesComida(comida,pais).subscribe(x =>{
+      if(x){
+        this.imagenesComida = x.hits;
+      }
+    });
+  }
 }
--- src/app/shared/service/paises/paises.service.ts
+++ src/app/shared/service/paises/paises.service.ts
@@ -1,30 +1,39 @@
 import { Injectable } from '@angular/core';
-import { HttpClient} from '@angular/common/http'
+import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { Paises } from './models/paises';
-import data from 'src/assets/data/countries.json'
-import dataComida from 'src/assets/data/countries-dishes.json'
+import data from 'src/assets/data/countries.json';
+import dataComida from 'src/assets/data/countries-dishes.json';
 import { ComidaPaises } from './models/comidaPaises';
+import { ThisReceiver } from '@angular/compiler';
+import { ImagenesComida } from './models/imagenesComida';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PaisesService {
+  constructor(private cliente: HttpClient) {}
 
-  constructor(private cliente: HttpClient) { }
-
-  obtenerPaises():Observable<Paises[]>{
-    let paises: Paises[] = data
-    return of(paises)
+  obtenerPaises(): Observable<Paises[]> {
+    let paises: Paises[] = data;
+    return of(paises);
     //return this.cliente.get<Paises[]>('api/paises');
   }
 
-  obtenerComidaPaises(pais:string){
-    let comidaPaises: ComidaPaises[] = dataComida
-    return of(comidaPaises.find(x => x.country == pais))
+  obtenerComidaPaises(pais: string) {
+    let comidaPaises: ComidaPaises[] = dataComida;
+    return of(comidaPaises.find((x) => x.country == pais));
     //return this.cliente.get<ComidaPaises[]>('api/comidaPaises');
-
   }
 
-
+  obtenerImagenesComida(comida: string, pais: string) {
+    let secureComida = comida.split(' ').join('');
+    secureComida = secureComida.split(',')[0];
+    let securePais = pais.split(' ').length > 1 ? '' : pais;
+    return this.cliente.get<ImagenesComida>(
+      `https://pixabay.com/api/?key=28768747-682b48a12e4746f0394313291&q=food+${secureComida}${
+        securePais != '' ? '+' + securePais : ''
+      }&image_type=photo&pretty=true&per_page=5`
+    );
+  }
 }
